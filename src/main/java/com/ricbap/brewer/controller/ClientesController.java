de96a8ba8d268843dfa138dd2f53c0fe8faaafa1
Ricardo <ricbapdevs@gmail.com>
2020-11-05 15:15:41 -0300

    Aplicando mascara de telefone no template de Cliente, utilizando uma biblioteca do Jquery, com o nome de jquery.mask.min.js

--- src/main/java/com/ricbap/brewer/controller/ClientesController.java
+++ src/main/java/com/ricbap/brewer/controller/ClientesController.java
@@ -1,34 +1,14 @@
 package com.ricbap.brewer.controller;
 
-import javax.validation.Valid;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import com.ricbap.brewer.model.Cliente;
 
 @Controller
-@RequestMapping("/clientes")
 public class ClientesController {
-	
-	@GetMapping("/novo")
-	public String novo(Cliente cliente) {
+
+	@RequestMapping("/clientes/novo")
+	public String novo() {
 		return "cliente/CadastroCliente";
 	}
 	
-	@PostMapping("/novo")
-	public String cadastrar(@Valid Cliente cliente, BindingResult result, RedirectAttributes redirectAttributes) {
-		if(result.hasErrors()) {			
-			return novo(cliente);
-		}
-		
-		redirectAttributes.addFlashAttribute("mensagem", "Cliente salvo com sucesso!");
-		System.out.println(">>>> Nome: " + cliente.getNome());
-		return "redirect:/clientes/novo";
-	}
-
 }
