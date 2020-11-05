18b46a8067dc6e71c7f9f0fa08778a3e539214ea
Ricardo <ricbapdevs@gmail.com>
2020-11-05 18:01:49 -0300

    Colocando mascara de CPF ou CNPJ em cliente, sando JavaScript

---
+++ src/main/resources/static/javascripts/cliente.mascara-cpf-cnpj.js
@@ -0,0 +1,30 @@
+var Brewer = Brewer || {};
+
+Brewer.MascaraCpfCnpj = (function() {
+	
+	function MascaraCpfCnpj() {
+		this.radioTipoPessoa = $('.js-radio-tipo-pessoa');
+		this.labelCpfCnpj = $('[for=cpfCnpj]');
+		this.inputCpfCnpj = $('#cpfCnpj');
+	}
+	
+	MascaraCpfCnpj.prototype.iniciar = function() {
+		this.radioTipoPessoa.on('change', onTipoPessoaAlterado.bind(this));
+	}
+	
+	function onTipoPessoaAlterado(event) {
+		var tipoPessoaSelecionada = $(event.currentTarget);
+		this.labelCpfCnpj.text(tipoPessoaSelecionada.data('documento'));
+		this.inputCpfCnpj.mask(tipoPessoaSelecionada.data('mascara'));
+		this.inputCpfCnpj.val(''); // apaga ao trocar
+		this.inputCpfCnpj.removeAttr('disabled');
+	}
+	
+	return MascaraCpfCnpj;
+	
+}());
+
+$(function() {
+	var mascaraCpfCnpj = new Brewer.MascaraCpfCnpj();
+	mascaraCpfCnpj.iniciar();
+});
