4c5a1693b46ddf005ba4f02bd1bfe90d99fe85d8
Ricardo <ricbapdevs@gmail.com>
2020-11-09 11:32:07 -0300

    Adicionando a coluna de cidade e estado na pesquisa de clientes

--- src/main/java/com/ricbap/brewer/repository/helper/cliente/ClienteRepositoryImpl.java
+++ src/main/java/com/ricbap/brewer/repository/helper/cliente/ClienteRepositoryImpl.java
@@ -6,23 +6,27 @@ import javax.persistence.PersistenceContext;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.MatchMode;
-import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
+import org.hibernate.sql.JoinType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 import com.ricbap.brewer.model.Cliente;
 import com.ricbap.brewer.repository.filter.ClienteFilter;
+import com.ricbap.brewer.repository.paginacao.PaginacaoUtil;
 
 public class ClienteRepositoryImpl implements ClienteRepositoryQuery {
 	
 	@PersistenceContext
 	private EntityManager manager;
+	
+	@Autowired
+	private PaginacaoUtil paginacaoUtil;
 
 	@SuppressWarnings("unchecked")
 	@Override
@@ -30,21 +34,25 @@ public class ClienteRepositoryImpl implements ClienteRepositoryQuery {
 	public Page<Cliente> filtrar(ClienteFilter filter, Pageable pageable) {
 		Criteria criteria = manager.unwrap(Session.class).createCriteria(Cliente.class);
 		
-		int paginaAtual = pageable.getPageNumber();
-		int totalRegistrosPorPagina = pageable.getPageSize();
-		int primeiroRegistro = paginaAtual * totalRegistrosPorPagina;
-		
-		criteria.setFirstResult(primeiroRegistro);
-		criteria.setMaxResults(totalRegistrosPorPagina);
-		
-		Sort sort = pageable.getSort();
-		if (sort != null) {
-			Sort.Order order = sort.iterator().next();
-			String propriedade = order.getProperty();
-			criteria.addOrder(order.isAscending() ? Order.asc(propriedade) : Order.desc(propriedade));
-		}
+		/*
+		 * int paginaAtual = pageable.getPageNumber(); int totalRegistrosPorPagina =
+		 * pageable.getPageSize(); int primeiroRegistro = paginaAtual *
+		 * totalRegistrosPorPagina;
+		 * 
+		 * criteria.setFirstResult(primeiroRegistro);
+		 * criteria.setMaxResults(totalRegistrosPorPagina);
+		 * 
+		 * Sort sort = pageable.getSort(); if (sort != null) { Sort.Order order =
+		 * sort.iterator().next(); String propriedade = order.getProperty();
+		 * criteria.addOrder(order.isAscending() ? Order.asc(propriedade) :
+		 * Order.desc(propriedade)); }
+		 */
+		paginacaoUtil.preparar(criteria, pageable);
 		
 		adicionarFiltro(filter, criteria);
+		
+		criteria.createAlias("endereco.cidade", "c", JoinType.LEFT_OUTER_JOIN);
+		criteria.createAlias("c.estado", "e", JoinType.LEFT_OUTER_JOIN);
 		
 		return new PageImpl<>(criteria.list(), pageable, total(filter));
 	}
