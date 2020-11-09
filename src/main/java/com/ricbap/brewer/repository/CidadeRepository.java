0d88d9d08c7b4f9c319d63654d75b95a03025aa0
Ricardo <ricbapdevs@gmail.com>
2020-11-09 15:05:18 -0300

    Cadastro de Cidades - Consulta em CidadesRepository por nome e estado para fazer validacao

--- src/main/java/com/ricbap/brewer/repository/CidadeRepository.java
+++ src/main/java/com/ricbap/brewer/repository/CidadeRepository.java
@@ -1,13 +1,17 @@
 package com.ricbap.brewer.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ricbap.brewer.model.Cidade;
+import com.ricbap.brewer.model.Estado;
 
 public interface CidadeRepository extends JpaRepository<Cidade, Long> {
 	
 	public List<Cidade> findByEstadoCodigo(Long codigoEstado);
+	
+	public Optional<Cidade> findByNomeAndEstado(String nome, Estado estado);
 	
 }
---
+++ src/main/java/com/ricbap/brewer/service/exception/NomeCidadeJaCadastradaException.java
@@ -0,0 +1,11 @@
+package com.ricbap.brewer.service.exception;
+
+public class NomeCidadeJaCadastradaException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public NomeCidadeJaCadastradaException(String mensagem) {
+		super(mensagem);
+	}
+
+}
