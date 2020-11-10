b76458273aa20448da2648279f6ddfd7ecee2b3a
Ricardo <ricbapdevs@gmail.com>
2020-11-09 22:13:40 -0300

    Prototipando pesquisa de cidades

--- src/main/java/com/ricbap/brewer/controller/CidadesController.java
+++ src/main/java/com/ricbap/brewer/controller/CidadesController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -70,7 +71,15 @@ public class CidadesController {
 		
 		attributes.addFlashAttribute("mensagem", "Cidade salva com sucesso!");
 		return new ModelAndView("redirect:/cidades/nova");		
-	}	
+	}
+	
+	@GetMapping
+	public ModelAndView pesquisar(Cidade cidade) {
+		ModelAndView mv = new ModelAndView("cidade/PesquisaCidades");
+		
+		mv.addObject("estados", estadoRepository.findAll());
+		return mv;
+	}
 	
 
 }
