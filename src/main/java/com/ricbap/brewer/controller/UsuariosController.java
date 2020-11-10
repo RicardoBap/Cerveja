375f83f29d8ff99515707835606bf85a85d04c33
Ricardo <ricbapdevs@gmail.com>
2020-11-10 20:04:35 -0300

    Validação parcial de envio de formulário de cadastro de usuario-

--- src/main/java/com/ricbap/brewer/controller/UsuariosController.java
+++ src/main/java/com/ricbap/brewer/controller/UsuariosController.java
@@ -1,19 +1,36 @@
 package com.ricbap.brewer.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.ricbap.brewer.model.Usuario;
 
 @Controller
 @RequestMapping("/usuarios")
 public class UsuariosController {
 	
 	@RequestMapping("/novo")
-	public ModelAndView novo() {
+	public ModelAndView novo(Usuario usuario) {
 		ModelAndView mv = new ModelAndView("usuario/CadastroUsuario");
 		
 		return mv;
-	}	
+	}
+	
+	@PostMapping("/novo")
+	public ModelAndView salvar(@Valid Usuario usuario, BindingResult result, RedirectAttributes redirectAttributes) {
+		if (result.hasErrors()) {
+			return novo(usuario);
+		}
+		
+		redirectAttributes.addFlashAttribute("mensagem",  "Usuario cadastrado com sucesso!");		
+		return new ModelAndView("redirect:/usuarios/novo");
+	}
 	
 
 }
