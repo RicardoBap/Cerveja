60a33a9fdea67c9d093be072e27817e7dd3495cc
Ricardo <ricbapdevs@gmail.com>
2020-11-12 19:04:15 -0300

    Prototipando a pagina de login e ativando as configuranções para fazer login

--- src/main/java/com/ricbap/brewer/config/SecurityConfig.java
+++ src/main/java/com/ricbap/brewer/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,16 +20,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.withUser("admin").password("admin").roles("CADASTRO_CLIENTE");
 	}
 	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+			.antMatchers("/layout/**")
+			.antMatchers("/images/**");			
+	}	
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-		  		.anyRequest().authenticated() 
-		  	.and()
-		  		.formLogin()
-		  	.and()
-		  		.csrf().disable();
+		  		.anyRequest().authenticated()
+		  		.and()
+		  	.formLogin()
+		  		.loginPage("/login")
+		  		.permitAll()
+		  		.and()
+		  	.csrf().disable();
 	}
 	 
 	
