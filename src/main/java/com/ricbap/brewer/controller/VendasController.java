236f5d1ab3f62a0e9193328211abe120c305c343
Ricardo <ricbapdevs@gmail.com>
2020-11-28 19:13:25 -0300

    Renderizando tabela de itens de venda - Foi criado um Template para renderização dessa tebela - TabelaItensVenda.html

--- src/main/java/com/ricbap/brewer/controller/VendasController.java
+++ src/main/java/com/ricbap/brewer/controller/VendasController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.ricbap.brewer.model.Cerveja;
 import com.ricbap.brewer.repository.CervejaRepository;
@@ -26,11 +27,13 @@ public class VendasController {
 	}
 	
 	@PostMapping("/item")
-	public @ResponseBody String adicionarItem(Long codigoCerveja) {
+	public ModelAndView adicionarItem(Long codigoCerveja) {
 		Cerveja cerveja = cervejaRepository.findOne(codigoCerveja);
 		tabelaItensVenda.adicionarItem(cerveja, 1);
-		System.out.println(">>>>>> total de itens: " + tabelaItensVenda.total());
-		return "Item adicionado";
+		ModelAndView mv = new ModelAndView("venda/TabelaItensVenda");
+		mv.addObject("itens", tabelaItensVenda.getItens());
+		
+		return mv;
 	}
 
 }
