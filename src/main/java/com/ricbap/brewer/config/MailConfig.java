561737183905815e0ce42bc6715bf4eeafbf9d78
Ricardo <ricbapdevs@gmail.com>
2020-12-02 09:05:15 -0300

    Emitindo

--- src/main/java/com/ricbap/brewer/config/MailConfig.java
+++ src/main/java/com/ricbap/brewer/config/MailConfig.java
@@ -27,7 +27,7 @@ public class MailConfig {
 		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
 		mailSender.setHost("smtp.gmail.com");
 		mailSender.setPort(587);
-		mailSender.setUsername("ricbapdevs@gmail.com");
+		mailSender.setUsername();
 		mailSender.setPassword(env.getProperty("password"));		
 		
 		System.out.println(">>> username: " + env.getProperty("username"));
